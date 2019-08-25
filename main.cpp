1c4dd956855d607507ed94a5a4f9c44c166ca640
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-08-24 19:03:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main()
+{
+    int lineas;
+    map<string, int> mapa;
+    string pais;
+    string temp;
+    
+    cin>>lineas;
+    for(int i=0;i<lineas;i++){
+        cin >> pais;
+        getline(cin, temp);
+        mapa[pais] ++;
+    }
+
+    for (auto ite = mapa.begin(); ite != mapa.end(); ite++)
+        cout<<ite->first<<" "<<ite->second<<endl;
+
+
 }
